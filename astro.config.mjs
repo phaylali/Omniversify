53a99ae43975dc099cc14284c01233564787543b
Phaylali <admin@omniversify.com>
2026-03-16 13:32:00 +0000

    feat: Integrate Astro Cloudflare adapter and configure Wrangler for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,14 @@ import react from '@astrojs/react';
 
 import pagefind from 'astro-pagefind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react(), pagefind()]
+  integrations: [react(), pagefind()],
+  adapter: cloudflare()
 });
